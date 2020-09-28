304d74914e40a9593fab217a3b4f7e12d8d4febf
Li Ning <csningli@gmail.com>
2020-09-28 18:11:34 +0800

    add monitor.py.

---
+++ disto/monitor.py
@@ -0,0 +1,26 @@
+
+import time
+from multiprocessing import Process
+
+from disto.utils import *
+
+def run_agent(agent, queue, timeout = 60) :
+    start_time = time.time()
+    while time.time() - start_time < timeout :
+        agent.run()
+
+
+class Monitor(object) :
+    def __init__(self) :
+        pass
+
+    def run(self, agents, timeout = 60) :
+        result = {"start" : get_current_time(), "finish" : None}
+        pool = [Process(target = run_agent, kwargs = {"agent" : agent, "timeout" : timeout}) for agent in agents]
+        for process in pool :
+            process.start()
+        for process in pool :
+            process.join()
+        result["finish"] = get_current_time()
+        result["asgns"] = {agent.id : agent.asgn for agent in agents}
+        return result
--- disto/utils.py
+++ disto/utils.py
@@ -1,8 +1,17 @@
 
-import os, datetime
+import os, datetime, queue
 
 def get_current_time() :
     return datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")
 
 def get_datetime_stamp() :
     return datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+def get_all_items_in_queue(q, max_num_items = 10):
+    items = []
+    for i in range(0, max_num_items):
+        try:
+            items.append(q.get_nowait())
+        except Empty, e:
+            break
+    return items

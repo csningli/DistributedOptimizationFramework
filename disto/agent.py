3d2cd21fe69b323d180fce8bca3b1e771b4f58b7
csningli <csningli@gmail.com>
2020-10-05 11:08:14 +0800

    working on SyncBTAgent.

--- disto/agent.py
+++ disto/agent.py
@@ -16,10 +16,24 @@ class Agent(object) :
 class SyncBTAgent(Agent) :
     def __init__(self, id, pro) :
         super(SyncBTAgent, self).__init__(id = id, pro = pro)
+        self.sorted_vars = sorted(list(self.pro.vars.keys()))
         self.assign = {}
         if self.pro is not None :
             for var, domain in self.pro.vars.items() :
                 self.assign[var] = domain.first_value()
+
+    def collect_utility(self) :
+        total = 0
+        return total
+
+    def fix_assign(self, var = None) :
+        result = False
+        if var is None and len(self.sorted_vars) > 0 :
+            var = self.sorted_vars[0]
+        if var is not None and None not in self.assign.values() :
+            
+        return False
+
 
     def process(self, msgs) :
         msgs = []

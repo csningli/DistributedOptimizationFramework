b00d3d13975c1d8aa5678e5f0e62a542d543609a
csningli <csningli@gmail.com>
2020-10-11 00:36:14 +0800

    working on AsyncBTAgent at first.

--- disto/agent.py
+++ disto/agent.py
@@ -13,14 +13,17 @@ class Agent(object) :
         msgs = []
         return {"msgs" : msgs}
 
-class SyncBTAgent(Agent) :
-    def __init__(self, id, pro) :
-        super(SyncBTAgent, self).__init__(id = id, pro = pro)
+class AsyncBTAgent(Agent) :
+    def __init__(self, id, pro, last_id) :
+        super(AsyncBTAgent, self).__init__(id = int(id), pro = pro)
+        self.last_id = last_id
         self.sorted_vars = sorted(list(self.pro.vars.keys()))
-        self.assign = {}
-        if self.pro is not None :
-            for var, domain in self.pro.vars.items() :
-                self.assign[var] = domain.first_value()
+        self.assign = {var : None for var in self.sorted_vars}
+        self.status = "created"
+
+    def next_assign(self) :
+        for var, domain in self.pro.vars.items() :
+            self.assign[var] = domain.first_value()
 
     def collect_utility(self) :
         total = 0
@@ -31,10 +34,12 @@ class SyncBTAgent(Agent) :
         if var is None and len(self.sorted_vars) > 0 :
             var = self.sorted_vars[0]
         if var is not None and None not in self.assign.values() :
-            
+
         return False
 
 
     def process(self, msgs) :
         msgs = []
+        if None in self.assign.values() :
+
         return {"msgs" : msgs}

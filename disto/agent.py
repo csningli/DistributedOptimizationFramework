41525c99aba93674a8aa2d3d5332b59f2fdf3f1f
NING Li <li.ning@siat.ac.cn>
2021-01-01 13:03:20 +0800

    add examples/SyncBB/main.py.

--- disto/agent.py
+++ disto/agent.py
@@ -87,6 +87,59 @@ class SyncBTAgent(Agent) :
                 self.log_msg("send", msg)
         return result
 
+class SyncBBAgent(Agent) :
+    def __init__(self, id, pro, prev, next, log_dir = "") :
+        super(SyncBBAgent, self).__init__(id = int(id), pro = pro, log_dir = log_dir)
+        self.prev = prev
+        self.next = next
+        self.sorted_vars = sorted(list(self.pro.vars.keys()))
+        self.cpa = {}
+
+    def process(self, msgs) :
+        result = {"msgs" : []}
+        if len(self.sorted_vars) > 0 :
+            if self.prev is None and self.assign[self.sorted_vars[0]] is None :
+                self.assign = self.init_assign(vars = self.pro.vars, order_list = self.sorted_vars)
+                self.assign, cost, violated = self.fix_assign(pro = self.pro, assign = self.assign, order_list = self.sorted_vars)
+                self.log("start/init cpa")
+                if cost is None :
+                    result["msgs"].append(SysMessage(src = self.id, content = None))
+                else :
+                    if self.next is None :
+                        result["msgs"].append(SysMessage(src = self.id, content = copy.deepcopy(cpa)))
+                        result["msgs"].append(SysMessage(src = self.id, content = None))
+                    else :
+                        result["msgs"].append(CommMessage(src = self.id, dest = self.next, content = copy.deepcopy(self.assign)))
+            elif len(msgs) > 0 :
+                msg = msgs[0]
+                self.log_msg("receive", msg)
+                if msg.src == self.prev :
+                    self.cpa = copy.deepcopy(msg.content)
+                    if self.assign[self.sorted_vars[0]] is None :
+                        self.assign = self.init_assign(vars = self.pro.vars, order_list = self.sorted_vars)
+                elif msg.src == self.next :
+                    self.assign = self.next_assign(self.assign, vars = self.pro.vars, order_list = self.sorted_vars)
+                cpa = {**self.cpa, **self.assign}
+                cpa, cost, violated = self.fix_assign(pro = self.pro, assign = cpa, order_list = self.sorted_vars)
+                if cost is None :
+                    self.assign = self.init_assign(vars = self.pro.vars, order_list = self.sorted_vars)
+                    if self.prev is None :
+                        result["msgs"].append(SysMessage(src = self.id, content = None))
+                        self.log_msg("send", result["msgs"][-1])
+                    else :
+                        result["msgs"].append(CommMessage(src = self.id, dest = self.prev, content = copy.deepcopy(cpa)))
+                else :
+                    for var in self.assign.keys() :
+                        self.assign[var] = cpa[var]
+                    if self.next is None :
+                        result["msgs"].append(SysMessage(src = self.id, content = copy.deepcopy(cpa)))
+                        result["msgs"].append(SysMessage(src = self.id, content = None))
+                    else :
+                        result["msgs"].append(CommMessage(src = self.id, dest = self.next, content = copy.deepcopy(cpa)))
+            for msg in result["msgs"] :
+                self.log_msg("send", msg)
+        return result
+
 class AsynBTAgent(Agent) :
     def __init__(self, id, pro, outgoings, var_host, con_host, log_dir = "") :
         super(AsynBTAgent, self).__init__(id = int(id), pro = pro, log_dir = log_dir)
--- examples/SyncBB/main.py
+++ examples/SyncBB/main.py
@@ -1,12 +1,69 @@
 
+import os, math
 import networkx as nx
 
+from disto.problem import total_cost, GraphColoringProblem
+from disto.agent import SyncBBAgent
 from disto.monitor import Monitor
-from disto.agent import SyncBTAgent
+from disto.utils import print_problem, get_datetime_stamp, view_logs
+
+# In this example, SyncBT (Makoto Yokoo and Toru Ishida, "The Distributed
+# Constraint Satisfaction Problem: Formalization and Algorithms", 1998)
+# is used to solve the graph coloring problem, which is modeled in a DCSP form.
 
 if __name__ == "__main__" :
-    # graph = nx.generators.random_graphs.fast_gnp_random_graph(n = 5, p = 0.6)
-    # result = color_graph(graph = graph, num_colors = 4)
-    agents = [SyncBTAgent(id = i) for i in range(10)]
-    Monitor().run(agents = agents, timeout = 5)
-    print("Done")
+    n = 6 # number of variables
+
+    # graph = nx.generators.random_graphs.fast_gnp_random_graph(n = n, p = 0.6)
+
+    graph = nx.Graph()
+    for i in range(n) :
+        graph.add_node(str(i))
+
+    d = 2
+    for i in range(n) :
+        for j in range(1, d + 1) :
+            graph.add_edge(str(i), str((i + j) % n))
+
+    # build the graph coloring problem in the DCSP format
+
+    pro = GraphColoringProblem(graph = graph, num_colors = 3, violation_cost = True)
+    print("* Global Problem *")
+    print_problem(pro = pro)
+    print("-" * 50)
+
+    m = 3 # number of the agents
+    avars = [[str(j) for j in range(n) if j % m == i] for i in range(m)]
+    # print(avars)
+
+    sub_pros = pro.split(avars = avars)
+    agents = []
+    log_dir = "logs/%s" % get_datetime_stamp()
+    if not os.path.isdir(log_dir) :
+        os.mkdir(log_dir)
+    for i in range(m) :
+        prev = i - 1 if i > 0 else None
+        next = i + 1 if i < m - 1 else None
+        agents.append(SyncBBAgent(id = i, pro = sub_pros[i], log_dir = log_dir, prev = prev, next = next))
+
+    for i, agent in enumerate(agents) :
+        print("Agent: %s" % agent.info())
+        print("* Sub-Problem: %d *" % i)
+        print_problem(pro = agent.pro)
+        print("-" * 50)
+
+    monitor = Monitor()
+    time_cost = monitor.run(agents = agents, timeout = 1)
+    print("Time cost: %s" % time_cost)
+    print("-" * 50)
+    print("Monitor.mem: %s" % monitor.mem)
+    print("-" * 50)
+    if len(monitor.mem) > 0 :
+        final = monitor.mem[-1]
+        print("Final: %s" % final)
+        cost, _ = total_cost(cons = pro.cons, assign = final)
+        print("Cost: %s" % cost)
+        print("-" * 50)
+    view_logs(log_dir = log_dir, style = "timeline")
+    print("-" * 50)
+    print("Done.")
